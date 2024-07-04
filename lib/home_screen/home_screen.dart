70ad222cd0a195332f1893725d909c5120f1e37d
r4yb3l-qa <raybel.hernandez@qa-bit.com>
2024-07-04 07:39:22 -0600

    11. Refactored main screen

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:excuela_widget_master/quiz_screen/bloc/quiz_screen_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,40 +9,73 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    context.read<QuizScreenBloc>().add(QuizScreenEvent.initialize());
     return Scaffold(
         appBar: AppBar(
-          title: Text('Excuela Widget Master'),
+          title: Image.network('https://excuela.com/excuela-logo.png'),
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(onPressed: (){
-                  context.pushNamed('loadingScreen');
-                }, child: const Text('Loading Screen')),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(onPressed: (){
-                  context.pushNamed('userCard');
-                }, child: const Text('User Card')),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(onPressed: (){
-                  context.pushNamed('quizScreen');
-                }, child: const Text('Quiz Screen')),
-              ],
-            ),
-          ],
-        )
-    );
+        body: Stack(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Opacity(
+                  opacity: 0.5,
+                  child: Image.network('https://excuela.com/interfaz-intro.png', height: size.height*0.8,)),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              Text(
+                'Widget Master',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(
+                height: size.height * 0.15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        context.pushNamed('loadingScreen');
+                      },
+                      child: const Text('Loading Screen')),
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        context.pushNamed('userCard');
+                      },
+                      child: const Text('User Card')),
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        context.pushNamed('quizScreen');
+                      },
+                      child: const Text('Quiz Screen')),
+                ],
+              ),
+            ],
+          ),
+        ]));
   }
 }
